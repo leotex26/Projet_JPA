6d9e08c29dfb64ab696c527113ea711e10fd06fb
leotex26 <leo.texier26@gmail.com>
2025-12-03 11:02:14 +0100

    premier commit

---
+++ src/test/java/fr/diginamic/MaClassTest.java
@@ -0,0 +1,44 @@
+package fr.diginamic;
+
+import fr.diginamic.service.AppService;
+import org.junit.jupiter.api.*;
+
+public class MaClassTest {
+
+//  @Test
+//  public void Test(){
+//    AppService appService = new AppService();
+//  }
+//
+//  @Test(expected = NullPointerException.class)
+//  public void TestNull(){
+//    AppService appService = new AppService();
+//  }
+//
+//  @Test(timeout = 1000)
+//  public void TestPerf(){
+//    AppService appService = new AppService();
+//  }
+//
+//  @BeforeAll
+//  public void ouvertureRessources(){
+//
+//  }
+//
+//  @AfterAll
+//  public void fermetureRessources(){
+//
+//  }
+//
+//  @BeforeEach
+//  public void beforeEach(){
+//
+//  }
+//
+//  @AfterEach
+//  public void beforeEach(){
+//
+//  }
+
+
+}
